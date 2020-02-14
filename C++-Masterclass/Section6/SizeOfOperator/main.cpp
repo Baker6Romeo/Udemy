8adb99330f003b9267010e39c698681ce997df19
baker6romeo <baker6romeo@gmail.com>
2020-02-14 15:46:47 -0500

    Saving work

--- C++-Masterclass/Section6/SizeOfOperator/main.cpp
+++ C++-Masterclass/Section6/SizeOfOperator/main.cpp
@@ -12,5 +12,18 @@ int main()
   cout << "sizeof information" << endl;
   cout << "======================================" << endl;
 
+  cout << "char: " << sizeof(char) << " bytes." << endl;
+  cout << "int: " << sizeof(int) << " bytes." << endl;
+  cout << "unsigned int: " << sizeof(unsigned int) << " bytes." << endl;
+  cout << "short: " << sizeof(short) << " bytes." << endl;
+  cout << "long: " << sizeof(long) << " bytes." << endl;
+  cout << "long long: " << sizeof(long long) << "bytes." << endl;
 
+  cout << "======================================" << endl;
+
+  cout << "float: " << sizeof(float) << " bytes." << endl;
+  cout << "double: " << sizeof(double) << " bytes." << endl;
+  cout << "long double:" << sizeof(long double) << " bytes." <<endl;
+
+  return 0;
 }
